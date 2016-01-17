3ce12f0d2ca64c3c54079e4ef7b52bf632d03889
Shivani R <shivanir@thoughtworks.com>
2016-01-17 16:47:31 +0530

    | 2 | Map multiple url's to a single method |

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -12,16 +12,25 @@ import java.util.List;
 @RequestMapping(value = "/api")
 public class HelloController {
     private Student st1;
+
     @RequestMapping(value = "/hello/{userName}/{userId}")
     public void welcomeMessage(@PathVariable("userName") String userName, @PathVariable("userId") int userId) {
         st1 = new Student(userName);
-        System.out.println("hello "+ st1.getName());
-        System.out.println("User Id: "+userId);
+        System.out.println("hello " + st1.getName());
+        System.out.println("User Id: " + userId);
     }
 
     @RequestMapping(value = "/subjects/{sub1}/{sub2}/{sub3}")
-    public void printSubjects(@PathVariable("sub1")String subject1, @PathVariable("sub2")String subject2,
-                              @PathVariable("sub3")String subject3) {
-        System.out.println("u study "+subject1 + " " + subject2 + " " + subject3);
+    public void printSubjects(@PathVariable("sub1") String subject1, @PathVariable("sub2") String subject2,
+                              @PathVariable("sub3") String subject3) {
+        if (subject3.length() > 0)
+            System.out.println("u study " + subject1 + " " + subject2 + " " + subject3);
+        else
+            System.out.println("u study " + subject1 + " " + subject2);
+    }
+
+    @RequestMapping(value = {"/method1", "/method1/method2"})
+    public void printMessage() {
+        System.out.println("This is to indicate that multiple url's are mapped to same method");
     }
 }
