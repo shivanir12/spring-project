9491442f9b0319b21b20ee593acb4ad22c5dd254
Shivani R <shivanir@thoughtworks.com>
2016-01-17 17:55:12 +0530

    | 4 | Methods can take Query parameters |

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,10 +1,7 @@
 package hello;
 
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,15 +11,15 @@ import java.util.List;
 public class HelloController {
     private Student st1;
 
-    @RequestMapping(value = "/hello/{userName}/{userId}", method = RequestMethod.GET)
-    public String welcomeMessage(@PathVariable("userName") String userName, @PathVariable("userId") int userId) {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String welcomeMessage(@RequestParam("userName") String userName, @RequestParam("userId") int userId) {
         st1 = new Student(userName);
         return "hello " + st1.getName() +"\n"+"User Id: " + userId;
     }
 
-    @RequestMapping(value = "/subjects/{sub1}/{sub2}/{sub3}", method = RequestMethod.GET)
-    public String printSubjects(@PathVariable("sub1") String subject1, @PathVariable("sub2") String subject2,
-                              @PathVariable("sub3") String subject3) {
+    @RequestMapping(value = "/subjects", method = RequestMethod.GET)
+    public String printSubjects(@RequestParam("sub1") String subject1, @RequestParam("sub2") String subject2,
+                              @RequestParam("sub3") String subject3) {
         if (subject3.length() > 0)
             return "u study " + subject1 + " " + subject2 + " " + subject3;
         else
@@ -30,6 +27,7 @@ public class HelloController {
     }
 
     @RequestMapping(value = {"/method1", "/method1/method2"}, method = RequestMethod.GET)
+    @ResponseBody
     public String printMessage() {
         return "This is to indicate that multiple url's are mapped to same method";
     }
