a173fedffc8420bf7818784bc625543fa4a42a83
Shivani R <shivanir@thoughtworks.com>
2016-01-17 22:22:09 +0530

    | 6 | Send the json for api /userapi/sendUser |

--- src/main/java/hello/UserController.java
+++ src/main/java/hello/UserController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,10 @@ public class UserController {
     @RequestMapping(value="/user", method = RequestMethod.POST, headers = {"Content-type=application/json"})
     public User getUser() {
         return new User("Shivani",15);
+    }
+
+    @RequestMapping(value="/sendUser", method = RequestMethod.POST, headers = {"Content-type=application/json"})
+    public User  setUser (@RequestBody User user) {
+        return user;
     }
 }
